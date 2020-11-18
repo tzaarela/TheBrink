4b54618483e7597f4724e82d821839b42d236a78
tzaarela <jimmysaarela@gmail.com>
2020-11-18 14:35:18 +0100

    added mono

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room
+public class Room : MonoBehaviour
 {
     public float AirLevel { get; set; }
     public float RadiationLevel { get; set; }
