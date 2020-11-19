fa67e4de1cf119a32d41fa18f104b98715ed5c3a
BakaRecca <eddie.norberg@gmail.com>
2020-11-19 14:46:12 +0100

    Created TestScript just for own testing purpose.

---
+++ Assets/TestScript.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField]
+    private CrewMember _crewMember;
+
+    [SerializeField]
+    private RoomController _roomController;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            
+        }
+    }
+}
