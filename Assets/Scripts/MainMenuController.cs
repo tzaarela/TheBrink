4ba253e0f804ed3d74f86a61efa64c64cb86e8fb
BakaRecca <eddie.norberg@gmail.com>
2020-11-24 17:10:06 +0100

    Base structure for main menu.
    Switch panels to view chosen content.

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -7,11 +7,19 @@ using TMPro;
 
 public class MainMenuController : MonoBehaviour
 {
+    public static MainMenuController Instance;
+    
     [SerializeField]
     private GameObject[] _panels;
+    
+    private MainMenuPanelType _currentPanel;
 
     private void Awake()
     {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
     }
 
     private void Start()
@@ -22,7 +30,8 @@ public class MainMenuController : MonoBehaviour
     private void Init()
     {
         HideAllPanels();
-        ShowPanel(MainMenuPanelType.Main);
+        _currentPanel = MainMenuPanelType.Main;
+        ShowPanel(_currentPanel);
     }
 
     private void HideAllPanels()
@@ -32,34 +41,73 @@ public class MainMenuController : MonoBehaviour
             panel.SetActive(false);
         }
     }
+    
+    public void ShowPanel(int panelIndex)
+    {
+        ShowPanelOnly(_currentPanel = (MainMenuPanelType)panelIndex);
+        Debug.Log($"ShowPanel: {_currentPanel}");
+    }
 
     private void ShowPanel(MainMenuPanelType panelType)
     {
-        
+        _panels[(int)panelType].SetActive(true);
+    }
+
+    private void ShowPanelOnly(MainMenuPanelType panelType)
+    {
+        HideAllPanels();
+        _panels[(int)panelType].SetActive(true);
     }
 
     public void NewOnClick()
     {
-        
+        ShowPanelOnly(_currentPanel = MainMenuPanelType.New);
     }
     
     public void LoadOnClick()
     {
-        
+        ShowPanelOnly(_currentPanel = MainMenuPanelType.Load);
     }
     
     public void OptionsOnClick()
     {
-        
+        ShowPanelOnly(_currentPanel = MainMenuPanelType.Options);
     }
     
     public void CreditsOnClick()
     {
-        
+        ShowPanelOnly(_currentPanel = MainMenuPanelType.Credits);
+    }
+
+    public void BackOnClick()
+    {
+        switch (_currentPanel)
+        {
+            case MainMenuPanelType.New:
+                Debug.Log($"Pressed Back from New");
+                ShowPanelOnly(_currentPanel = MainMenuPanelType.Main);
+                break;
+            case MainMenuPanelType.Load:
+                Debug.Log($"Pressed Back from Load");
+                ShowPanelOnly(_currentPanel = MainMenuPanelType.Main);
+                break;
+            case MainMenuPanelType.Options:
+                Debug.Log($"Pressed Back from Options");
+                ShowPanelOnly(_currentPanel = MainMenuPanelType.Main);
+                break;
+            case MainMenuPanelType.Credits:
+                Debug.Log($"Pressed Back from Credits");
+                ShowPanelOnly(_currentPanel = MainMenuPanelType.Main);
+                break;
+        }
     }
     
     public void ExitOnClick()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
