37e520c2ececf1a385f1851c368d3af8c5204179
BakaRecca <eddie.norberg@gmail.com>
2020-12-04 12:37:40 +0100

    save commit (spaceport for testing...!)

---
+++ Assets/SpaceportUITest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceportUITest : MonoBehaviour
+{
+    [SerializeField] private GameObject _tempCanvas;
+    [SerializeField] private GameObject _spaceportCanvas;
+    [SerializeField] private GameObject _contractsPanel;
+    [SerializeField] private GameObject _contractsInfoPanel;
+    [SerializeField] private GameObject _barrackPanel;
+    
+}
