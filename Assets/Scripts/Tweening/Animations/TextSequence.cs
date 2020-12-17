bfad8fa8ed25caa7df2f101afe910db5b03d9dbf
tzaarela <jimmysaarela@gmail.com>
2020-12-17 08:52:34 +0100

    Queue system for text sequences

---
+++ Assets/Scripts/Tweening/Animations/TextSequence.cs
@@ -0,0 +1,43 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Assets.Scripts.Tweening.Animations
+{
+    [Serializable]
+    public class TextSequence : MonoBehaviour, ITweenObject
+    {
+        public float timeInSeconds;
+        public float delayInSeconds;
+
+        private Text uiText;
+        private string endValue;
+        private float timeUntilNextTween;
+
+        public void Start()
+        {
+            uiText = GetComponent<Text>();
+            endValue = uiText.text;
+            uiText.text = "";
+        }
+
+        public float CompletionTime
+        {
+            get { return timeInSeconds + delayInSeconds; }
+        }
+
+        public float TimeUntilNextTween
+        {
+            get { return timeUntilNextTween; }
+        }
+
+        public void ExecuteTween()
+        {
+            uiText.DOText(endValue, timeInSeconds).SetDelay(delayInSeconds);
+        }
+    }
+}
