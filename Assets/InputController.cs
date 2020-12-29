78aabb01add96451d6a267c5c06f115bbcf2bcab
tzaarela <jimmysaarela@gmail.com>
2020-12-29 14:00:40 +0100

    better  input handling with scene transitions

---
+++ Assets/InputController.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public bool clickToContinue;
+    public bool anyKey;
+    public KeyCode keyCode;
+    public GameScene gameScene;
+
+    private ClickToContinue ctc;
+
+    void Update()
+    {
+        if(clickToContinue)
+        {
+            if(ctc == null)
+            {
+                if (anyKey)
+                    ctc = new ClickToContinue(gameScene);
+                else
+                    ctc = new ClickToContinue(gameScene, keyCode);
+            }
+
+            ctc.CheckInput();
+        }
+    }
+}
