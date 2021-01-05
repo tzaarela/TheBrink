c02b7bcf465a3ada6adce8cd21fcc133e1dffedd
BakaRecca <eddie.norberg@gmail.com>
2021-01-05 11:49:11 +0100

    options wip.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -9,6 +9,10 @@ using TMPro;
 public class MenuController : MonoBehaviour
 {
     private GameObject _menuPanel;
+
+    [SerializeField] private GameObject _systemFilesPanel;
+    [SerializeField] private GameObject _optionsPanel;
+
     private void Awake()
     {
         GetAllComponents();
@@ -18,7 +22,7 @@ public class MenuController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && GameController.Instance.GameScene != GameScene.MainMenu)
             ToggleMenu();
     }
 
@@ -30,15 +34,50 @@ public class MenuController : MonoBehaviour
     public void ToggleMenu()
     {
         _menuPanel.SetActive(!_menuPanel.activeInHierarchy);
+
+        if (_menuPanel.activeInHierarchy)
+            ShowSystemFiles();
     }
 
     public void Options()
     {
-        
+        ShowOptions();
     }
 
     public void ExitToMainMenu()
     {
         GameController.Instance.GameScene = GameScene.MainMenu;
+    }
+
+    private void ShowSystemFiles()
+    {
+        _optionsPanel.SetActive(false);
+        _systemFilesPanel.SetActive(true);
+    }
+
+    public void ConfirmOptions()
+    {
+        Debug.Log($"GameController.Instance.GameScene: {GameController.Instance.GameScene}");
+        if (GameController.Instance.GameScene == GameScene.MainMenu)
+            CloseOptions();
+        else
+            ShowSystemFiles();
+    }
+
+    private void ShowOptions()
+    {
+        _systemFilesPanel.SetActive(false);
+        _optionsPanel.SetActive(true);
+    }
+
+    public void OpenOptions()
+    {
+        _menuPanel.SetActive(true);
+        ShowOptions();
+    }
+
+    private void CloseOptions()
+    {
+        _menuPanel.SetActive(false);
     }
 }
